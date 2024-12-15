8361581db359fb38d232b1b6240b0ae47f2b91d1
wexquix00 <alibaba31697231@gmail.com>
2024-12-15 19:20:17 +0400

    Create ip.py

---
+++ ip.py
@@ -0,0 +1,9 @@
+import requests
+
+b = "\033[92mdeveloper: t.me/wexquix\nkanal: t.me/wexquixPython"
+print(b)
+a = "https://api.ipify.org"
+response = requests.get(a)
+s = response.text
+print('\n')
+print(f'\033[96mip adresin: {s}')
